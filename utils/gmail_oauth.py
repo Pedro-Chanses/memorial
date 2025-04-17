b1a0828ad15a7f843c92fede3e07e23235e20a07
mashmanuc <mashmanuc@gmail.com>
2025-04-17 22:26:48 +0300

    Переробив oAuch

--- utils/gmail_oauth.py
+++ utils/gmail_oauth.py
@@ -20,7 +20,10 @@ def create_oauth_flow(for_gmail=False):
             "redirect_uris": [
                 "http://127.0.0.1:5000/auth/oauth2callback",
                 "http://localhost:5000/auth/oauth2callback",
-                "https://okinava.onrender.com/auth/oauth2callback"
+                "https://memorial-05p8.onrender.com/auth/oauth2callback",
+                "https://memorial-05p8.onrender.com/auth/authorize",
+                "https://memorial-app.herokuapp.com/auth/authorize",
+                "https://memorial-app.herokuapp.com/auth/oauth2callback"
             ]
         }
     }
